9f9b6dd61003ff41f51fd97a2acca6420871dbaf
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2023-08-24 13:29:38 +0100

    calculator

---
+++ calculator-app/src/component/Calculator.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+const Calculator = () => {
+  return (
+    <div>
+      <div></div>
+      <div></div>
+      <div>
+        <button>AC</button>
+        <button>DEL</button>
+        <button>+</button>
+        <button className='bg-white'>-</button>
+        <button ></button>
+        <button>1</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>.</button>
+        <button>0</button>
+        <button>=</button>
+      </div>
+    </div>
+  )
+}
+
+export default Calculator
+
